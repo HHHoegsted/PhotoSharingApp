be3df4b349a33f1b6b69cb43a98a36fbde9d6caf
hhhoegsted <hh.hoegsted@gmail.com>
2019-09-10 09:16:23 +0200

    Photo and Comment classes added

--- PhotoSharingApplication/Models/Comment.cs
+++ PhotoSharingApplication/Models/Comment.cs
@@ -7,5 +7,11 @@ namespace PhotoSharingApplication.Models
 {
     public class Comment
     {
+        public int CommentID { get; set; }
+        public int PhotoID { get; set; }
+        public string UserName { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
     }
 }
